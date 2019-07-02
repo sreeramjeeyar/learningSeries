73486869ff6293f7d2c129e7b791c8a6f5f486e1
sreeram jeeyar <sreeramjeeyar@gmail.com>
2019-07-03 00:40:31 +0530

    Add files via upload

---
+++ c++/comparisonopp.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+
+int main(int argc,char **argv)
+{
+   int x=10;
+   int y=10;
+   if (x!=y)
+    {
+           cout<<"x is not equal to y"<<endl;
+       } 
+       
+       else if(x==y)
+   {
+
+       cout<<"x is equal to y"<<endl;
+   }
+     
+     if(x>y){
+       cout<<"x is greater"<<endl;
+     }
+       else if(x<y)
+       {
+           cout<<"x is smaller than y"<<endl;
+       }  
+       
+       
+       
+       return 0;
+       
+   }        
+
+
+
+
