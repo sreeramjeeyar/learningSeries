2e47dc63e0fcb61949dc8c2e72780ea369155493
sreeram jeeyar <sreeramjeeyar@gmail.com>
2019-07-04 23:31:42 +0530

    subscript

---
+++ c++/subscript.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+
+int main( int argc, char ** argv ) {
+
+ int ia[]={1,2,3,4,5};
+ string ib="sreeram";
+ int i=4;
+ int x=3;
+ printf("element is %d\n",ia[i]);
+ printf("element is %c\n",ib[x]);
+
+	return 0;
+}
